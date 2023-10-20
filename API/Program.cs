44f18270f597b956dd3da8b31581fbd8b68c9969
zzfuture <113058239+zzfuture@users.noreply.github.com>
2023-10-20 13:34:56 -0500

    feat: :sparkles: creating the project
    API, Core & Infrastructure

---
+++ API/Program.cs
@@ -0,0 +1,46 @@
+using System.Reflection;
+using API.Extensions;
+using AspNetCoreRateLimit;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ikerropaContext>(optionsBuilder =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MySqlConex");
+    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+builder.Services.ConfigureCors();
+builder.Services.AddApplicationServices();
+builder.Services.ConfigureRateLimiting();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("CorsPolicy");
+app.UseIpRateLimiting();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
