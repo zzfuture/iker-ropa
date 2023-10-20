6a93469062c63d7b1eeffb360482adcdb04e60ec
zzfuture <113058239+zzfuture@users.noreply.github.com>
2023-10-20 13:41:25 -0500

    feat: :sparkles: MappingProfiles Implemented

---
+++ API/Profiles/MappingProfiles.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
+using Core.Models;
+
+namespace API.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles() // Remember adding : Profile in the class
+        {
+            CreateMap<Cargo, CargoDto>().ReverseMap();
+            CreateMap<Cliente, ClienteDto>().ReverseMap();
+            CreateMap<Color, ColorDto>().ReverseMap();
+            CreateMap<Departamento, DepartamentoDto>().ReverseMap();
+            CreateMap<DetalleOrden, DetalleOrdenDto>().ReverseMap();
+            CreateMap<DetalleVenta, DetalleVentaDto>().ReverseMap();
+            CreateMap<Empleado, EmpleadoDto>().ReverseMap();
+            CreateMap<Empresa, EmpresaDto>().ReverseMap();
+            CreateMap<Estado, EstadoDto>().ReverseMap();
+            CreateMap<FormaPago, FormaPagoDto>().ReverseMap();
+            CreateMap<Genero, GeneroDto>().ReverseMap();
+            CreateMap<Insumo, InsumoDto>().ReverseMap();
+            CreateMap<InsumoPrenda, InsumoPrendaDto>().ReverseMap();
+            CreateMap<InsumoProveedor, InsumoProveedorDto>().ReverseMap();
+            CreateMap<Inventario, InventarioDto>().ReverseMap();
+            CreateMap<InventarioTalla, InventarioTallaDto>().ReverseMap();
+            CreateMap<Municipio, MunicipioDto>().ReverseMap();
+            CreateMap<Orden, OrdenDto>().ReverseMap();
+            CreateMap<Pais, PaisDto>().ReverseMap();
+            CreateMap<Prenda, PrendaDto>().ReverseMap();
+            CreateMap<Proveedor, ProveedorDto>().ReverseMap();
+            CreateMap<Talla, TallaDto>().ReverseMap();
+            CreateMap<TipoEstado, TipoEstadoDto>().ReverseMap();
+            CreateMap<TipoPersona, TipoPersonaDto>().ReverseMap();
+            CreateMap<TipoProteccion, TipoProteccionDto>().ReverseMap();
+            CreateMap<Venta, VentaDto>().ReverseMap();
+        }
+    }
+}
